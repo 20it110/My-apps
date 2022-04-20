442d4390f28ea9bd778f18f5dc5ad27292a43f92
Rudra Patel <20it110@charusat.edu.in>
2022-04-20 21:14:36 +0530

    hyy

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quizzler());
+QuizBrain quizBrain=QuizBrain();
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Quizzz App")),
+          backgroundColor: Colors.teal,
+        ),
+        backgroundColor: Colors.teal[700],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  List<Icon> score=[];
+
+  void checkans(bool userpickedans){
+    bool correctans=quizBrain.gerCorrectans();
+    setState(() {
+      if(quizBrain.isFinished()==true){
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+        quizBrain.reset();
+        score= [];
+      }
+      else{
+        if(userpickedans==correctans)
+        {
+          score.add(Icon(Icons.check,color: Colors.green,));
+        }
+        else{
+          score.add(Icon(Icons.close,color: Colors.red,));
+        }
+        setState(() {
+          quizBrain.nextQue();
+        });
+      }
+    });
+
+
+
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQueText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkans(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkans(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: score,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ lib/questions.dart
@@ -0,0 +1,9 @@
+class Questions{
+  String questiontext;
+  bool questionans;
+
+  Questions(String q, bool a){
+    questiontext=q;
+    questionans=a;
+  }
+}
